6583c4019053477d1f925691b568321a0fa192c2
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-10 12:05:45 +0600

    examples click on

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ let example2Button = document.getElementById('example2');
 example2Button.onclick = function() {
     alert("Nice to meet you!");
 }
+
+let example3Button = document.getElementById('example3');
+example3Button.addEventListener('click', function() {
+    alert("Nice to meet you!");
+})
+
+example3Button.addEventListener('click', function() {
+    alert("Bye!")
+})
+
+let example4Button = document.getElementById('example4');
+example4Button.addEventListener('click', function() {
+    example4Button.textContent = "Nice to meet you!";
+})
